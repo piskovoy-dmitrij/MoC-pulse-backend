976bf624db0e31a6a680f4703a4479f5a29efb4d
Sergey Gychka <sergey.gychka@gmail.com>
2015-07-04 22:21:29 +0300

    load users from auth token

--- storage/storage.go
+++ storage/storage.go
@@ -2,12 +2,10 @@ package storage
 
 import (
 	"encoding/json"
-	"fmt"
 	"errors"
-	"gopkg.in/redis.v3"
+
 	"github.com/piskovoy-dmitrij/MoC-pulse-backend/auth"
-//	"strconv"
-//	"time"
+	"gopkg.in/redis.v3"
 )
 
 type Vote struct {
@@ -32,9 +30,6 @@ func ConnectToRedis() *redis.Client {
 		DB:       0,  // use default DB
 	})
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	
 	return client
 }
 
--- storage/users.go
+++ storage/users.go
@@ -4,33 +4,37 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/parnurzeal/gorequest"
 	"github.com/piskovoy-dmitrij/MoC-pulse-backend/auth"
-//	"strconv"
-//	"time"
 	"log"
 )
 
+var endpoint string = "http://192.168.4.121:3000/api/trusted/profiles.json?name=PulsePush&token=1-K_I6DY1QbNknp-MXHN4QDhTmD1BQCgyesCoHfZExzABKdwvKOenIcisq7UubPprAcnZBrpP4qmu-j-nzlH_F8A"
+
+type UserInterface interface {
+}
+
 func SaveUser(user auth.User) {
 	client := ConnectToRedis()
-	
-	// retain readability with json
-    serialized, err := json.Marshal(user)
 
-    if err == nil {
-        fmt.Println("serialized data: ", string(serialized))
-		
-		err := client.Set("user:" + user.Id, string(serialized), 0).Err()
-	    if err != nil {
-	        log.Fatal("Failed to set user into redis: ", err)
-	    }
+	// retain readability with json
+	serialized, err := json.Marshal(user)
+
+	if err == nil {
+		fmt.Println("serialized data: ", string(serialized))
+
+		err := client.Set("user:"+user.Id, string(serialized), 0).Err()
+		if err != nil {
+			log.Fatal("Failed to set user into redis: ", err)
+		}
 	}
-	
+
 	client.Close()
-} 
+}
 
 func SaveAuthToken(at auth.AuthToken) {
 	client := ConnectToRedis()
-	
+
 	// retain readability with json
 	serialized, err := json.Marshal(at)
 
@@ -46,18 +50,21 @@ func SaveAuthToken(at auth.AuthToken) {
 
 //func GetAuthToken(token string) *auth.AuthToken {
 //	client := ConnectToRedis()
-	
-	
+
 //}
 
 func GetAllUsers() []auth.User {
 	client := ConnectToRedis()
-	
-	users_keys, _ := client.Keys("user:*").Result()
+
+	users_keys, err := client.Keys("user:*").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
 	var users []auth.User
 	for _, value := range users_keys {
+		fmt.Println(value)
 		item, err := LoadUser(value)
-		if err != nil {
+		if err == nil {
 			users = append(users, *item)
 		}
 	}
@@ -68,15 +75,48 @@ func GetAllUsers() []auth.User {
 
 func LoadUser(id string) (*auth.User, error) {
 	client := ConnectToRedis()
-	
+
 	data, err := client.Get(id).Result()
 	client.Close()
-	
+
 	if err != nil {
 		return nil, errors.New("Not exist")
 	} else {
 		user := &auth.User{}
 		json.Unmarshal([]byte(data), &user)
 		return user, nil
+	}
+}
+
+func GetUsers() ([]auth.User, error) {
+	_, body, errs := gorequest.New().Get(endpoint).End()
+	if errs != nil {
+		return nil, errors.New("Can't get users from Auth provider")
+	} else {
+
+		var loaded []auth.User
+		json.Unmarshal([]byte(body), &loaded)
+
+		exist_users := GetAllUsers()
+
+		var user_keys map[string]auth.User = map[string]auth.User{}
+
+		for _, value := range exist_users {
+			user_keys[value.Id] = value
+		}
+
+		var users []auth.User
+
+		for _, value := range loaded {
+			existed, ok := user_keys[value.Id]
+			fmt.Println(ok)
+			if !ok {
+				users = append(users, value)
+			} else {
+				users = append(users, existed)
+			}
+		}
+
+		return users, nil
 	}
 }
