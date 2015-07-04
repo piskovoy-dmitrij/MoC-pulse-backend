669db337abca4f2578442ca57e025d5b05b96d61
Dmitrij Piskovoj <dmytro.piskovyi@masterofcode.com>
2015-07-04 13:33:26 +0300

    adding simple ini config

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/FogCreek/mini"
+)
+
+func fatal(v interface{}) {
+	fmt.Println(v)
+}
+
+func chk(err error) {
+	if err != nil {
+		fatal(err)
+	}
+}
+
+func params() string {
+	cfg, err := mini.LoadConfiguration(".pulseconfigrc")
+
+    chk(err)
+
+    fmt.Printf("params")
+
+
+	info := fmt.Sprintf("db=%s",
+		cfg.String("db", "127.0.0.1"),
+	)
+	return info
+}
 
 func main() {
     fmt.Printf("hello, world\n")
 }
+
