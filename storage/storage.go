2f048d2b6ba5844a4036e5386478a1277c20822b
Sergey Gychka <sergey.gychka@gmail.com>
2015-07-04 20:15:42 +0300

    load all users and load user

--- storage/storage.go
+++ storage/storage.go
@@ -1,12 +1,13 @@
 package storage
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/piskovoy-dmitrij/MoC-pulse-backend/auth"
 	"gopkg.in/redis.v3"
-	"encoding/json"
 	"strconv"
 	"time"
-	"fmt"
 )
 
 type Vote struct {
@@ -26,83 +27,115 @@ type VoteResult struct {
 
 func ConnectToRedis() {
 	client := redis.NewClient(&redis.Options{
-	    Addr:     "localhost:6379",
-	    Password: "", // no password set
-	    DB:       0,  // use default DB
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
 	})
-	
+
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 }
 
 func NewVote(name string, owner string) *Vote {
 	id := strconv.FormatInt(time.Now().UnixNano(), 10)
-	
+
 	vote := &Vote{
 		Name:  name,
 		date:  time.Now().UnixNano(),
 		owner: owner,
 		Id:    id,
-	} 
-	
-	client := redis.NewClient(&redis.Options{
-	    Addr:     "localhost:6379",
-	    Password: "", // no password set
-	    DB:       0,  // use default DB
-	})
-	
-	// retain readability with json
-    serialized, err := json.Marshal(vote)
-
-    if err == nil {
-        fmt.Println("serialized data: ", string(serialized))
-		
-		
-		err := client.Set(id, string(serialized), 0).Err()
-	    if err != nil {
-	        panic(err)
-	    }
 	}
-	
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// retain readability with json
+	serialized, err := json.Marshal(vote)
+
+	if err == nil {
+		fmt.Println("serialized data: ", string(serialized))
+
+		err := client.Set(id, string(serialized), 0).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	return vote
 }
 
 func SaveUser(user auth.User) {
 	client := redis.NewClient(&redis.Options{
-	    Addr:     "localhost:6379",
-	    Password: "", // no password set
-	    DB:       0,  // use default DB
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
 	})
-	
-	// retain readability with json
-    serialized, err := json.Marshal(user)
 
-    if err == nil {
-        fmt.Println("serialized data: ", string(serialized))
-		
+	// retain readability with json
+	serialized, err := json.Marshal(user)
+
+	if err == nil {
+		fmt.Println("serialized data: ", string(serialized))
+
 		err := client.Set(user.Id, string(serialized), 0).Err()
-	    if err != nil {
-	        panic(err)
-	    }
+		if err != nil {
+			panic(err)
+		}
 	}
-} 
+}
 
 func SaveAuthToken(at auth.AuthToken) {
 	client := redis.NewClient(&redis.Options{
-	    Addr:     "localhost:6379",
-	    Password: "", // no password set
-	    DB:       0,  // use default DB
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
 	})
-	
-	// retain readability with json
-    serialized, err := json.Marshal(at)
 
-    if err == nil {
-        fmt.Println("serialized data: ", string(serialized))
-				
+	// retain readability with json
+	serialized, err := json.Marshal(at)
+
+	if err == nil {
+		fmt.Println("serialized data: ", string(serialized))
+
 		err := client.Set(at.HMAC, string(serialized), 0).Err()
-	    if err != nil {
-	        panic(err)
-	    }
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func GetAllUsers() []auth.User {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	users_keys := client.Keys("user:*")
+	var users []auth.User
+	for _, value := range users_keys {
+		item, err := LoadUser(value)
+		if err != nil {
+			users = append(users, item)
+		}
+	}
+	return users
+}
+
+func LoadUser(id string) (auth.User, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	data, err := client.Get(id).Result()
+	if err != nil {
+		return nil, errors.New("Not exist")
+	} else {
+		user := auth.User{}
+		json.Unmarshal([]byte(data), &user)
+		return user, nil
 	}
 }
