56489f102bde11626e6c9336e75bd2bc9718c451
Vitalii Hurin <vitalii.hurin@masterofcode.com>
2016-05-16 19:21:31 +0300

    Fix log leveling

--- log/logger.go
+++ log/logger.go
@@ -3,6 +3,7 @@ package log
 import (
 	"log"
 	"os"
+	"io/ioutil"
 )
 
 var Error *log.Logger
@@ -13,10 +14,10 @@ var file *os.File
 
 func NewLogger(logLevel int64, logPath string) {
 	var err error
-	Error = log.New(nil, "ERROR: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-	Warning = log.New(nil, "WARNING: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-	Info = log.New(nil, "INFO: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-	Debug = log.New(nil, "DEBUG: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	Error = log.New(ioutil.Discard, "ERROR: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	Warning = log.New(ioutil.Discard, "WARNING: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	Info = log.New(ioutil.Discard, "INFO: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	Debug = log.New(ioutil.Discard, "DEBUG: ", log.Ldate|log.Lmicroseconds|log.Lshortfile)
 	if logLevel >= 1 {
 		file, err = os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
